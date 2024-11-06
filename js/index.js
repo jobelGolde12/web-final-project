c850feade994a0fb088ad16748c3fb5303b8016d
jobel v. golde <jobelgolde43@gmail.com>
2024-11-06 20:55:43 +0800

    Preparing last page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('.video-list');
-    list .addEventListener('click', () => {
+    list.addEventListener('click', () => {
         window.location.href = '../pages/videos/videos.html';
 
     })
+})
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('.bi-house');
+  list.addEventListener('click', () => {
+      window.location.href = '../../index.html';
+
+  })
 })
 window.addEventListener('scroll', () => {
     const logo = document.querySelector('.logo');
